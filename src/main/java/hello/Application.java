128bd3bd19ee9fbeedb886dd381d3069dee5c446
Raúl Baeza Osuna <95641914+RaulBaeza15@users.noreply.github.com>
2023-04-13 21:15:11 +0200

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -6,12 +6,4 @@ public class Application {
  public static void main(String[] args) {
  SpringApplication.run(Application.class, args);
  }
-}package hello;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class Application {
-	 public static void main(String[] args) {
-		  SpringApplication.run(Application.class, args);
-		   }
 }
